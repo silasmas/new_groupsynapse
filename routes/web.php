3fbb195fde89cf94a43a43f93c4bde85f8e91826
silasmas <silasjmas@gmail.com>
2025-03-15 11:33:59 +0100

    sc

--- routes/web.php
+++ routes/web.php
@@ -77,5 +77,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
+Route::get('/symlink', action: function () {
+    return view('symlink');
+})->name('generate_symlink');
 require __DIR__ . '/auth.php';
