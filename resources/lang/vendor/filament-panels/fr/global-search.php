e4e0beabb5342a9fabe3c602a95b8fddd489956c
silasmas <silasjmas@gmail.com>
2026-03-14 16:34:42 +0100

    ok

---
+++ resources/lang/vendor/filament-panels/fr/global-search.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'field' => [
+        'label' => 'Recherche globale',
+        'placeholder' => 'Utilisateurs, produits, services, commandes, transactions...',
+    ],
+
+    'no_results_message' => "Désolé, aucun résultat n'a été trouvé.",
+
+];
